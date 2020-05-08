ed638abd8992117dafc0de5fe6acba6ff95e2394
german <heinz.german@gmail.com>
2020-05-08 10:45:38 +0200

    Start with Basic Page

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:design/src/pages/basic_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Design',
+      initialRoute: 'basic',
+      routes: {
+        'basic': (BuildContext context) => BasicPage(),
+      },
+    );
+  }
+}
