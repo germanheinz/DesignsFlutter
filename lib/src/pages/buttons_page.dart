71976e389a2d8748168afd6f22e28d8cfaca387d
german <heinz.german@gmail.com>
2020-05-11 17:19:46 +0200

    Design part7

---
+++ lib/src/pages/buttons_page.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ButtonsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+            _background(),
+        ],
+      ),
+    );
+  }
+  Widget _background(){
+    final gradient = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.6),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52,54,101,1.0),
+            Color.fromRGBO(35, 37, 57, 1.0),
+          ] 
+        ) 
+      ),
+    );
+
+    final pinkBox = Transform.rotate(
+      angle: -pi / 5.0, 
+      child: Container(
+      height: 360.0,
+      width: 360.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50.0),
+        gradient: LinearGradient(
+        colors: [
+          Color.fromRGBO(236, 98, 188, 1.0),
+          Color.fromRGBO(236, 98, 188, 1.0)
+        ]
+        )
+      ),
+      ),
+    );
+    return Stack(
+      children: <Widget>[
+        gradient,
+        Positioned(
+          top: -100.0,
+          child: pinkBox)
+      ],
+    );
+  }
+}
